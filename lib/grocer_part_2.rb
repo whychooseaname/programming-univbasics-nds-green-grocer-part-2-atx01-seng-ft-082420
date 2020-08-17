4324b1b29ea0cce7d62dee88c3bb0a747c0fa5d4
Edward Acosta <edward.ea.acosta@gmail.com>
2020-08-17 06:59:26 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -55,5 +55,10 @@ def checkout(cart, coupons)
   cart = consolidate_cart(cart)
   cart = apply_coupons(cart,coupons)
   cart = apply_clearance(cart)
-
+  
+  cart.each do |food, info|
+    result += (info[:price] * info[:count]).to_f
+  end
+  result > 100 ? result * 0.9 : result
+end
 end
